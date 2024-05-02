1dddd5d05fbebb1dfc5df2088a07127ab1099b85
Cosmo <sokolily08@gmail.com>
2024-05-02 15:48:27 +0300

    update cosmo-bot

--- cosmo-bot.py
+++ cosmo-bot.py
@@ -1,5 +1,18 @@
 import asyncio
 from telethon import TelegramClient, errors
+import logging
+
+
+# Настройка логгирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Создание файла лога
+file_handler = logging.FileHandler('output.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+# Добавление файла лога в логгер
+logging.getLogger('').addHandler(file_handler)
 
 api_id = '23538569'  # Ваш API ID
 api_hash = '1c7d17a813d63199ff41ee4b46e22a50'  # Ваш API Hash
@@ -22,7 +35,7 @@ async def main():
         try:
             source_chat = await client.get_entity(source_chat_urls[0])  # Берем первую ссылку из списка
         except errors.ChannelPrivateError:
-            print(f'Не удалось получить доступ к исходному чату {source_chat_urls[0]}.')
+            logging.info(f'Не удалось получить доступ к исходному чату {source_chat_urls[0]}.')
             return  # Прерываем работу скрипта, если не получается доступ к исходному чату
 
         while True:  # Внешний бесконечный цикл
@@ -31,21 +44,21 @@ async def main():
                     target_chat = await client.get_entity(target_chat_url)
                     try:
                         await client.forward_messages(target_chat, photo_message_id, source_chat)
-                        print(f'Фото с ID {photo_message_id} успешно отправлено в {target_chat.title}.')
+                        logging.info(f'Фото с ID {photo_message_id} успешно отправлено в {target_chat.title}.')
                     except errors.SlowModeWaitError as e:
-                        print(f'Необходимо подождать {e.seconds} секунд прежде чем отправить следующее сообщение в {target_chat.title}.')
+                        logging.info(f'Необходимо подождать {e.seconds} секунд прежде чем отправить следующее сообщение в {target_chat.title}.')
                         continue  # Переход к следующему чату
                     except errors.ForbiddenError:
                         try:
                             await client.forward_messages(target_chat, alternative_message_id, source_chat)
-                            print(f'Альтернативное сообщение с ID {alternative_message_id} успешно отправлено в {target_chat.title}.')
+                            logging.info(f'Альтернативное сообщение с ID {alternative_message_id} успешно отправлено в {target_chat.title}.')
                         except Exception as e:
-                            print(f'Ошибка при отправке альтернативного сообщения в {target_chat.title}: {e}')
+                            logging.info(f'Ошибка при отправке альтернативного сообщения в {target_chat.title}: {e}')
                     except errors.ChannelPrivateError:
-                        print(f'Канал или чат {target_chat_url} является приватным или нет доступа.')
+                        logging.info(f'Канал или чат {target_chat_url} является приватным или нет доступа.')
                         continue  # Переход к следующему чату
                 except Exception as e:
-                    print(f'Ошибка при доступе к каналу или чату {target_chat_url}: {e}')
+                    logging.info(f'Ошибка при доступе к каналу или чату {target_chat_url}: {e}')
                 
                 await asyncio.sleep(1)  # Задержка перед отправкой следующего сообщения
 
